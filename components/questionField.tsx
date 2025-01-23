2c42b6404ee3fc59d84913046461b28002343332
IXIN <16cosu05mo30@gmail.com>
2025-01-24 02:25:33 +0900

    コピー不可

--- components/questionField.tsx
+++ components/questionField.tsx
@@ -11,7 +11,7 @@ const QuestionField = ({ data }: Props) => {
   if (!data) return null;
   return (
     <div className="gap-1 border border-sky-500 w-4/5 bg-sky-100 max-w-[600px] rounded-lg shadow-lg grid grid-rows-2 h-full p-1">
-      <div className="row-span-1 flex justify-center ">
+      <div className="row-span-1 flex justify-center">
         <Image
           className=" shadow-lg rounded-lg"
           width={500}
@@ -23,11 +23,11 @@ const QuestionField = ({ data }: Props) => {
       </div>
       <div className="row-span-1 grid grid-cols-4">
         <div className="col-span-3 overflow-y-scroll">
-          <p className="text-lg whitespace-pre-wrap">
+          <p className="text-lg whitespace-pre-wrap select-none">
             {convertDescription(data.description)}
           </p>
         </div>
-        <div className="col-span-1 overflow-y-scroll">
+        <div className="col-span-1 overflow-y-scroll select-none">
           <p className="text-sm text-center py-2 font-bold underline">
             Title
           </p>
