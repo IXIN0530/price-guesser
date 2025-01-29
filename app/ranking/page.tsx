435d9bd219f57aa98bc9180c487a207e825b8bda
IXIN <16cosu05mo30@gmail.com>
2025-01-30 01:52:43 +0900

    ランキング表示機能

--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -4,7 +4,9 @@ import functions from "@/components/functions";
 import { globalRankingType } from "@/type";
 import axios from "axios";
 import { param } from "framer-motion/m";
+import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import GRankElement from "@/components/ranking/GRankElement";
 
 export default function Page() {
   //現在表示するランキングのデータ
@@ -48,10 +50,14 @@ export default function Page() {
     const nowDate = new Date(params.date);
     //n日進める
     nowDate.setDate(nowDate.getDate() + num);
-    console.log(params.date)
     //新しいparamとしてdataFetchを呼び出す
     //ただし、前のdatafetchの呼び出しが完了してからに限る
     if (!isFetching.current) {
+      //未来に行こうとした場合
+      if (nowDate > date) {
+        alert("未来のデータは取得できません。")
+        return;
+      }
       dataFetch({
         date: nowDate,
         getAll: 0
@@ -82,7 +88,7 @@ export default function Page() {
       if (isFetched(params) != null) {
         console.log("すでに取得済みでした")
         const temp: FetchedData = isFetched(params)!
-        console.log(temp);
+        // console.log(temp);
         setRankingData(temp.data);
         isFetching.current = false;
         return;
@@ -92,11 +98,7 @@ export default function Page() {
       const res_data: globalRankingType[] = res.data["data"]
       //表示データに格納
       setRankingData(res_data);
-      //取得済みのデータに格納
-      // setFetchedData([...fetchedData, {
-      //   date: params.getAll == 1 ? null : params.date,
-      //   data: res_data,
-      // }]);
+      //取得済みデータに格納
       fetchedData.current = [...fetchedData.current,
       {
         date: params.getAll == 1 ? null : params.date,
@@ -118,13 +120,45 @@ export default function Page() {
     }
   }, [])
   return (
-    <div>
-      <button onClick={() => changeDate(-1)}>＜-</button>
-      <button onClick={() => changeDate(1)}>-＞</button>
+    <div className="min-h-[100svh]  mx-2 relative">
+      <div className=" grid grid-rows-11 max-w-[1000px] mx-auto absolute inset-0">
+        <div className="row-span-1 my-auto  text-xl flex flex-col justify-center">
+          <p className="text-xl text-center font-bold">ランキング</p>
+          <p className="text-lg text-center">{params.date.getFullYear() + "年" + params.date.getMonth() + 1 + "月" + params.date.getDate() + "日"}</p>
+        </div>
+        <div className="row-span-1 flex flex-row justify-center gap-2 my-2">
+          <button onClick={() => changeDate(-1)} className="px-3 bg-gradient-to-br from-orange-400 to-orange-300 rounded-lg shadow-lg text-white font-bold text-xl">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+          </button>
+          <button onClick={() => changeDate(1)} className="px-3 bg-gradient-to-br from-orange-400 to-orange-300 rounded-lg shadow-lg text-white font-bold text-xl">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+            </svg>
+          </button>
+        </div>
+        <div className=" relative row-span-7 items-center overflow-y-scroll border-2 border-orange-300 bg-gradient-to-br from-orange-100 to-orange-50 ">
+          <div className=" absolute inset-0">
+            {rankingData.map((score, index) => {
+              return (
+                <GRankElement scoreData={score} index={index} key={index} />
+              )
+            })}
+          </div>
+        </div>
+        <div className="row-span-2 my-auto">
+          <Link href={"./"} className=" flex flex-row justify-center gap-1 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+            <p className="text-xl">Back</p>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
-
 type Params = {
   date: Date,
   getAll: number,
---
+++ components/ranking/GRankElement.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { useState } from "react";
+import { AnimatePresence, color, motion } from "framer-motion";
+import { globalRankingType, LocalRankingType } from "@/type";
+import functions from "../functions";
+type Props = {
+  scoreData: globalRankingType,
+  index: number,
+  key: number
+}
+const GRankElement = ({ scoreData, index }: Props) => {
+  const { pointColor } = functions()
+  const [isClicked, setIsClicked] = useState(false);
+  //スコア情報、プレイヤーの名前の文字列を整形する関数
+  const formatScore = (detail: string) => {
+    const scoreArray = detail.split(",");
+    return { scoreDetail: scoreArray.slice(0, 5).map(e => Number(e)), name: scoreArray[5] }
+  }
+  //Playerのスコア情報と名前
+  const { scoreDetail, name } = formatScore(scoreData.scoreDetail!);
+  //後ろにth,st,nd,rdをつける関数
+  const addSuffix = (num: number) => {
+    if (num == 1) return num + "st";
+    else if (num == 2) return num + "nd";
+    else if (num == 3) return num + "rd";
+    else return num + "th";
+  }
+  return (
+    <div className="flex py-4 justify-center gap-5 flex-col border-b-2 border-slate-500"
+      onClick={() => setIsClicked(!isClicked)}
+      style={{ gridRow: !isClicked ? "span 1" : "span 2" }}>
+      <motion.div className="grid grid-cols-11  items-center "
+      >
+        <div className="col-span-1"></div>
+        <p className="col-span-1 text-center  text-3xl select-none ">
+          {addSuffix(index + 1)}
+        </p>
+        <div className="col-span-2"></div>
+        <p className="col-span-3 text-center text-2xl font-bold select-none">{scoreData.score!.toFixed(2)}</p>
+        <div className=" relative col-span-3 select-none overflow-x-scroll ">
+          <p className=" text-center">
+            {name}
+          </p>
+        </div>
+        <div className="col-span-1 mx-auto">
+          <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6"
+            initial={{ rotate: 0, opacity: 1 }}
+            animate={{ rotate: isClicked ? 90 : 0 }}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
+          </motion.svg>
+        </div>
+      </motion.div>
+      <AnimatePresence>
+        {isClicked && (
+          <motion.div
+            initial={{ opacity: 0, scaleY: 0 }}
+            animate={{ opacity: 1, scaleY: 1 }}
+            exit={{ opacity: 0, transition: { duration: 0 } }}
+            transition={{ duration: 0.3 }}
+            className=" flex flex-col justify-center mx-2">
+            <div className="grid grid-cols-10 items-center h-full ">
+              <p className="font-bold text-2xl col-span-2 ">score:</p>
+              {scoreDetail.length > 0 ?
+                <div className="col-span-4   h-full grid grid-rows-2 overflow-x-scroll">
+                  <div className="row-span-1 flex justify-center gap-2 font-bold">
+                    <p style={{ color: pointColor(scoreDetail[0]) }}>{scoreDetail[0].toFixed(1)}</p>
+                    <p style={{ color: pointColor(scoreDetail[1]) }}>{scoreDetail[1].toFixed(1)}</p>
+                    <p style={{ color: pointColor(scoreDetail[2]) }}>{scoreDetail[2].toFixed(1)}</p>
+                  </div>
+                  <div className="row-span-1 flex justify-center gap-2 font-bold">
+                    <p style={{ color: pointColor(scoreDetail[3]) }}>{scoreDetail[3].toFixed(1)}</p>
+                    <p style={{ color: pointColor(scoreDetail[4]) }}>{scoreDetail[4].toFixed(1)}</p>
+                  </div>
+                </div> : <div></div>}
+              <p className="col-span-3 text-center overflow-hidden">{scoreData.year + "/" + scoreData.month + "/" + scoreData.day}</p>
+              <p className="col-span-1 text-center overflow-hidden"></p>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
+
+export default GRankElement;
